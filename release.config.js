f9fb01974a6c267cab4da6d1da6b02ffdad4b828
Vladimir Sigalkin <ext@iron-wall.org>
2024-06-13 23:34:55 +0300

    fix: ci config

--- release.config.js
+++ release.config.js
@@ -23,11 +23,6 @@ module.exports = {
 		["@semantic-release/git", {
 			"assets": [`Assets/${process.env.PROJECT_NAME}/package.json`, "CHANGELOG.md"],
 			"message": "chore(release): new release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-		}],
-		["@semantic-release/github", {
-			"assets": [
-				{"path": `${process.env.PROJECT_NAME}.unitypackage`, "label": `${process.env.PROJECT_NAME} v\${nextRelease.version}`}
-			]
 		}]
 	],
 	"preset": "angular"
